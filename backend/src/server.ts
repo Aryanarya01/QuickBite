4c95f4725eddcc00ec78a195b966382db7a67f48
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 18:01:59 +0530

    setting server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,3 +3,7 @@ import dotenv from "dotenv"
 const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
