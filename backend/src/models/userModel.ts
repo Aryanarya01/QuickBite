21ee0204408ab8549ddff36b2e9570ec570ecf17
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 18:41:23 +0530

    user model  completed

--- backend/src/models/userModel.ts
+++ backend/src/models/userModel.ts
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 import type { IUser } from "../types/types.js";
 
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
 
-const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-    },
-    email : {
-        type : String,
-        required : true,
-        unique : true,
-    },
-    password : {
-        type :String,
-        required : true,
-    },
-    role : {
-        type : String,
-        enum : ["user","admin"],
-        default : "user",
-    },
-},
-{timestamps : true})
-
-export const User = mongoose.model<IUser>("User",userSchema);
- 
+export const User = mongoose.model<IUser>("User", userSchema);
