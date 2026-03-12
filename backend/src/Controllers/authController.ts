b855ac148d5ab33f3136fd708494bc694081b7d6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 21:50:27 +0530

    auth controller

--- backend/src/Controllers/authController.ts
+++ backend/src/Controllers/authController.ts
@@ -2,5 +2,13 @@ import type { NextFunction, Request, Response } from "express";
 
 
 export const register = async(req:Request,res:Response,next:NextFunction)=>{
-    
+    try{
+        const {name,email,password} = req.body;
+        if(!name||!email||!password){
+            res.status(400).json({message : "All fields required!"});
+            return;
+        }
+    }catch(err){
+        res.status(500).json({message : "Server Error!"});
+    }
 }
