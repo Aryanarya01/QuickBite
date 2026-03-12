61ee2efeef167aaa22d9129e2d2caa27c0b43bac
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 18:54:12 +0530

    food model comlpeted

--- backend/src/models/foodModel.ts
+++ backend/src/models/foodModel.ts
@@ -1,31 +1,31 @@
 import mongoose from "mongoose";
 import { IFood } from "../types/types.js";
 
+const foodSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
-const foodSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-        trim : true,
-    },
-    price : {
-        type : Number,
-        required : true,
-    },
-    description : {
-        type : String,
-        required : true,
-    },
-    image : {
-        type : String,
-        required : true,
-    },
-    category : {
-        type : String,
-        required : true,
-    }
-}
-,
-{timestamps : true})
-
-export const Food = mongoose.model<IFood>("Food",foodSchema);
+export const Food = mongoose.model<IFood>("Food", foodSchema);
