2185197bc82894fe5907adf79eed1670f3709824
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 18:38:25 +0530

    user model types

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,8 +1,6 @@
-
-
-export interface IUser extends Document{
-    name : string,
-    email : string,
-    password : string,  
-    
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "user" | "admin";
 }
