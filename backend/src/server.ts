9d53520c920a10abd85b01f360a2fecc6c537c3f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 18:07:11 +0530

    setting up database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv"
+import {connectDB} from "./config/db.js"
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
-
+connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+
+app.listen(port,()=>{
+    console.log(`App is listining to port ${port}`);
+})
