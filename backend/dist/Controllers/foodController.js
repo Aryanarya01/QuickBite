565659d2819a0f7ec0d01ed2f8f1d943c2058e6c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 23:54:16 +0530

    foodControllre

--- backend/dist/Controllers/foodController.js
+++ backend/dist/Controllers/foodController.js
@@ -1,3 +1,5 @@
+import { Food } from "../models/foodModel.js";
+import { json } from "node:stream/consumers";
 export const createFood = async (req, res) => {
     try {
         const { name, price, description, image, category } = req.body;
@@ -5,6 +7,16 @@ export const createFood = async (req, res) => {
             res.status(400).json({ message: "All fields are required" });
             return;
         }
+        const food = await Food.create({
+            name,
+            price,
+            description,
+            image,
+            category,
+        });
+        res.status(201).json({ message: "Successfully Created!",
+            food
+        });
     }
     catch (err) {
         res.status(500).json({ message: "Error Creating Food!" });
--- backend/src/Controllers/foodController.ts
+++ backend/src/Controllers/foodController.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from "express";
+import { Food } from "../models/foodModel.js";
+import { json } from "node:stream/consumers";
 
 
 
@@ -9,6 +11,16 @@ export const createFood = async(req:Request,res:Response)=>{
             res.status(400).json({message:"All fields are required"});
             return;
         }
+        const food = await Food.create({
+            name,
+            price,
+            description,
+            image,
+            category,
+        });
+        res.status(201).json({message : "Successfully Created!",
+            food
+        })
     }catch(err){
         res.status(500).json({message : "Error Creating Food!"});
     }
