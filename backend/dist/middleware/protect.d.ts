f316a68d175d7b9a2ddc7528f6a46480feb29b45
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 23:17:41 +0530

    Protect middleware

--- backend/dist/middleware/protect.d.ts
+++ backend/dist/middleware/protect.d.ts
@@ -1,2 +1,7 @@
-export declare const Protect: any;
+import type { NextFunction, Request, Response } from "express";
+interface AuthRequest extends Request {
+    user?: any;
+}
+export declare const Protect: (req: AuthRequest, res: Response, next: NextFunction) => Promise<void>;
+export {};
 //# sourceMappingURL=protect.d.ts.map
--- backend/src/middleware/protect.ts
+++ backend/src/middleware/protect.ts
@@ -1,6 +1,28 @@
-import type { Request } from "express";
-
+import type { NextFunction, Request, Response } from "express";
+import { User } from "../models/userModel.js";
+import jwt from "jsonwebtoken"
 interface AuthRequest extends Request{
     user?: any;
 }
-export const Protect = async()
+export const Protect = async(req:AuthRequest,res:Response,next:NextFunction)=>{
+    try{
+        const token = req.cookies.token;
+            if(!token){
+                res.status(400).json({message : "Not Authorized!"});
+                return;
+            }
+
+            const decoded = jwt.verify(token,process.env.JWT_SECRET as string) as {id : string};
+
+            const user = await User.findById(decoded.id).select("-password");
+            if(!user){
+                res.status(400).json({message:"User not found!"});
+                return;
+            }
+            req.user = user;
+            next()
+
+    }catch(err){
+        res.status(500).json({message : "Server Error!"});
+    }
+}
