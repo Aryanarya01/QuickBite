7b544acee78c782a4d8a85edf68449b191e9a267
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 21:38:20 +0530

    order model completed

--- backend/src/models/orderModel.ts
+++ backend/src/models/orderModel.ts
@@ -1,43 +1,45 @@
 import mongoose from "mongoose";
 import { IOrder } from "../types/types.js";
 
-const orderSchema = new mongoose.Schema({
-    user :{
-            type : mongoose.Types.ObjectId,
-            ref : "User",
-            required : true,
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    items: [
+      {
+        food: {
+          type: mongoose.Types.ObjectId,
+          ref: "Food",
+          required: true,
         },
-        items : [
-            {
-                food:{
-                    type:mongoose.Types.ObjectId,
-                    ref : "Food",
-                    required : true,
-                },
-                quantity :{
-                    type:Number,
-                    required : true,
-                },
-                price : {
-                    type : Number,
-                    required : true,
-                },
-            },
-        ],
-        totalPrice : {
-            type :Number,
-            required: true,
+        quantity: {
+          type: Number,
+          required: true,
         },
-        address : {
-            type : String,
-            required : true,
+        price: {
+          type: Number,
+          required: true,
         },
-        status : {
-            type : String,
-            enum : ["pending","confirmed","delivered"],
-            default : "pending",
-        },
-},
-{timestamps : true})
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "delivered"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
 
-export const Order = mongoose.model<IOrder>("Order",orderSchema);
+export const Order = mongoose.model<IOrder>("Order", orderSchema);
