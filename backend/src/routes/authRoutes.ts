b738b4d158a3275d3143216ad77282154d685bd1
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 22:52:49 +0530

    auth routes completed

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { Login, Logout, Register } from "../Controllers/authController.js";
 
-const router :Router = Router();
-router.post("/register",Register);
-router.post("/login",Login);
-router.post("/logout",Logout);
+const router: Router = Router();
+router.post("/register", Register);
+router.post("/login", Login);
+router.post("/logout", Logout);
