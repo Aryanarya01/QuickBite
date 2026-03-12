dec6b57bdd201372b5c2ce6c0c990556baa75b1f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-12 19:14:04 +0530

    order  model types

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -6,23 +6,23 @@ export interface IUser extends Document {
   role: "user" | "admin";
 }
 
-export interface IFood extends Document{
-    name : string,
-    price : number,
-    description : string,
-    image : string,
-    category : string,
+export interface IFood extends Document {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  category: string;
 }
 
-export interface IItem extends Document{
-    food : mongoose.Types.ObjectId,
-    quantity : number,
-    price : number,
+export interface IItem extends Document {
+  food: mongoose.Types.ObjectId;
+  quantity: number;
+  price: number;
 }
-export interface IOrder extends Document{
-     user : mongoose.Types.ObjectId,
-     items : IItem[],
-     totalPrice : number,
-     address :string,
-     status : "pending"|"confirmed"|"delivered";
+export interface IOrder extends Document {
+  user: mongoose.Types.ObjectId;
+  items: IItem[];
+  totalPrice: number;
+  address: string;
+  status: "pending" | "confirmed" | "delivered";
 }
