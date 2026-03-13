9e09cc5e132cc696680b699ce6a95bed976c3d90
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-13 21:53:09 +0530

    orderRoute completed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import authRouter from "./routes/authRoutes.js"
-import foodRouter from "./routes/foodRoutes.js"
-import orderRouter from "./routes/orderRoutes.js"
+import authRouter from "./routes/authRoutes.js";
+import foodRouter from "./routes/foodRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 import cookieParser from "cookie-parser";
-
 
 const app = express();
 const port = process.env.PORT || 5000;
 dotenv.config();
 connectDB();
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth",authRouter);
-app.use("/api/foods",foodRouter);
-app.use("/api/orders",orderRouter);
-app.get("/",(req,res)=>{
+app.use("/api/auth", authRouter);
+app.use("/api/foods", foodRouter);
+app.use("/api/orders", orderRouter);
+app.get("/", (req, res) => {
   res.send("working!");
-})
-
+});
 
 app.listen(port, () => {
   console.log(`App is listining to port ${port}`);
