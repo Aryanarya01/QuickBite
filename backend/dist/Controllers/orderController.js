cb950f7a7c812aa4ec136d3c6a78f6eb0f9f9057
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-13 18:01:46 +0530

    orderController createOrder

--- backend/dist/Controllers/orderController.js
+++ backend/dist/Controllers/orderController.js
@@ -1,5 +1,10 @@
 export const createOrder = async (req, res, next) => {
     try {
+        const { items, address } = req.body;
+        if (!items || items.length === 0) {
+            res.status(400).json({ message: "No items in orders!" });
+            return;
+        }
     }
     catch (err) {
         res.status(500).json({ message: "Order Creation Failed!!" });
--- backend/src/Controllers/orderController.ts
+++ backend/src/Controllers/orderController.ts
@@ -5,7 +5,11 @@ interface AuthRequest extends Request{
 }
 export const createOrder = async(req:AuthRequest,res:Response,next:NextFunction)=>{
     try{
-
+        const {items,address} = req.body;
+        if(!items || items.length === 0){
+            res.status(400).json({message : "No items in orders!"});
+            return;
+        }
     }catch(err){
         res.status(500).json({message : "Order Creation Failed!!"});
     }
