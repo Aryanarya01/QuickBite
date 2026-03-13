384db565a3eb7c84f21c424028a22137ce59b273
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-13 21:38:30 +0530

    orderController updateOrderStatus

--- backend/src/Controllers/orderController.ts
+++ backend/src/Controllers/orderController.ts
@@ -81,8 +81,14 @@ export const updateOrderStatus = async(req:AuthRequest,res:Response,next:NextFun
     const order = await Order.findByIdAndUpdate(req.params.id,
       {status},{new : true}
     );
-    
+    if(!order){
+      res.status(404).json({message : "Order not found!"});
+      return;
+    }
+    res.status(200).json({message : "Order Updated!",
+      order
+    })
   }catch(err){
-    res.status(500).json({message : "Error Updating Status!"});
+    res.status(500).json({message : "Error Updating order!"});
   }
 }
