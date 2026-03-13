8b227b66cc23508137b874f2fe569a5d564f43d8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-13 21:51:42 +0530

    orderRoute

--- backend/src/routes/orderRoutes.ts
+++ backend/src/routes/orderRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Protect } from "../middleware/protect.js";
-import { createOrder, GetMyOrder } from "../Controllers/orderController.js";
+import { createOrder, getAllOrder, GetMyOrder, updateOrderStatus } from "../Controllers/orderController.js";
+import { isAdmin } from "../middleware/admin.js";
 
 
 const router:Router = Router();
@@ -8,3 +9,8 @@ const router:Router = Router();
 
 router.post("/",Protect,createOrder);//user order
 router.get("/my",Protect,GetMyOrder);//user orders
+
+router.get("/",Protect,isAdmin,getAllOrder);//admin
+router.put("/:id",Protect,isAdmin,updateOrderStatus);//update status
+
+export default router;
