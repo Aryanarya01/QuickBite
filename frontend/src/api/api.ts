d7b9bae98b23692de11c67dbd86ff5eac00acc22
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 17:50:52 +0530

    api.ts setup completed

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,18 +1,17 @@
-
 export const API_URL = "http://localhost:5000/api";
 
-export const apiFetch = async (url:string,options:RequestInit={})=>{
-    const res = await fetch(`${API_URL}${url}`,{
-        credentials : "include",
-        headers : {
-            "Content-Type" : "application/json",
-        },
-        ...options,
-    });
-    const data = await res.json();
+export const apiFetch = async (url: string, options: RequestInit = {}) => {
+  const res = await fetch(`${API_URL}${url}`, {
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...options,
+  });
+  const data = await res.json();
 
-    if(!res.ok){
-        throw new Error(data.message || "Something went wrong!");
-    }
-    return data;
-}
+  if (!res.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+  return data;
+};
