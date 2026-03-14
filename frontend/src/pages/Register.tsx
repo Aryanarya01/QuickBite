448c964136405db97831adbeacab18ef1fc35aa6
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:20:38 +0530

    Register states

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = ()=>{
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate();
     return(
         <>
         </>
