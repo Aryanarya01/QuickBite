8c1dad5fbfc88c3704366ecd89a7015861ce9e60
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:15:09 +0530

    ProtectedRoute

--- frontend/src/Components/ProtectedRoute.tsx
+++ frontend/src/Components/ProtectedRoute.tsx
@@ -1,10 +1,18 @@
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext"
 
 const ProtectedRoute = ({children}:{children:JSX.Element})=>{
     const {user,loading} = useAuth();
 
+    if(loading){
+        return <h2>Loading...</h2>
+    }
 
+    if(!user){
+        return <Navigate to="/login" replace />
+    }
 
     return children;
+    
 }
 
