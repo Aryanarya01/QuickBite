9e04b6ad3a225dcb14616c2dcce5752aec200758
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 22:44:44 +0530

    authContext type

---
+++ frontend/src/types/AuthContexTypes.ts
@@ -0,0 +1,8 @@
+import type { User } from "./User";
+
+export interface AuthContextType{
+    user : User|null;
+    setUser : React.Dispatch<React.SetStateAction<User | null>>;
+    loading : boolean;
+    logout : ()=>Promise<void>;
+}   
