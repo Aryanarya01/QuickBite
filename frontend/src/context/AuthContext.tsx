6c273acd67282b44e04dd28f9a1e1507e21a3af7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 22:48:08 +0530

    authContext

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react"
+import { createContext, useState, type ReactNode } from "react"
 import type { User } from "../types/User"
+import type { AuthContextType } from "../types/AuthContexTypes";
 
-
-export const Authprovider = ({}:{})=>{
+export const AuthContext = createContext<AuthContextType|null>(null)
+export const Authprovider = ({children}:{children : ReactNode})=>{
     const [user,setUser] = useState<User|null>(null);
     const [loading,setLoading] = useState<boolean>(true);
 
 
-    //
+    //  Auto login check
 }
