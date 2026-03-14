c2ebcbed9363608e5f951f6f6116d0928d457f54
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 17:49:21 +0530

    api.ts setup

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,13 @@
 
 export const API_URL = "http://localhost:5000/api";
 
-export const apiFetch = (url:string,options)
+export const apiFetch = async (url:string,options:RequestInit={})=>{
+    const res = await fetch(`${API_URL}${url}`,{
+        credentials : "include",
+        headers : {
+            "Content-Type" : "application/json",
+        },
+        ...options,
+    });
+    const data = await res.
+}
