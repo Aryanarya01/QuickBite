949aa2a77b04ef2fc4951547ea0abe30f971a6d5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:01:49 +0530

    AuthContext comlpeted

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,56 +1,60 @@
-import { createContext, useContext, useEffect, useState, type ReactNode } from "react"
-import type { User } from "../types/User"
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from "react";
+import type { User } from "../types/User";
 import type { AuthContextType } from "../types/AuthContexTypes";
 import { apiFetch } from "../api/api";
 import { data } from "react-router-dom";
 
-export const AuthContext = createContext<AuthContextType|null>(null)
-export const Authprovider = ({children}:{children : ReactNode})=>{
-    const [user,setUser] = useState<User|null>(null);
-    const [loading,setLoading] = useState<boolean>(true);
+export const AuthContext = createContext<AuthContextType | null>(null);
+export const Authprovider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
+  //  Auto login check
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const res = await apiFetch("/auth/profile");
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data.user);
+        }
+      } catch (err) {
+        console.log("Not Logged in!");
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkUser();
+  }, []);
 
-    //  Auto login check
-    useEffect(()=>{
-        const checkUser = async ()=>{
-            try{
-                const res = await apiFetch("/auth/profile");
-                if(res.ok){
-                    const data = await res.json();
-                    setUser(data.user);
-                }
-            }catch(err){
-                console.log("Not Logged in!");
-                setUser(null);
-            }finally{
-                setLoading(false);
-            }
-        };
-        checkUser();
-    },[])
+  const logout = async () => {
+    await apiFetch("/auth/logout", {
+      method: "POST",
+    });
+    setUser(null);
+  };
 
-
-    const logout = async ()=>{
-        await apiFetch("/auth/logout",{
-            method : "POST"
-        });
-        setUser(null)
-    }
-
-    return(
-        <AuthContext.Provider value={{loading,logout,user,setUser}}>
-            {!loading&&children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ loading, logout, user, setUser }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
 
 //              custom hook
-export const useAuth = ()=>{
-    const context = useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
 
-    if(!context){
-        throw new Error("useAuth must be used inside AuthProvider!");
-    }
+  if (!context) {
+    throw new Error("useAuth must be used inside AuthProvider!");
+  }
 
-    return context;
-}
+  return context;
+};
