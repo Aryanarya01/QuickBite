53ca9519c603166c163a5a400e40737fb9663581
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 00:00:19 +0530

    Profile page completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import ProtectedRoute from './Components/ProtectedRoute'
+import Profile from './pages/Profile'
 
 function App() {
    
@@ -12,7 +14,9 @@ function App() {
        <Routes>
         <Route path='/register' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
-        
+        <Route path='/profile' element={<ProtectedRoute>
+          <Profile/>
+        </ProtectedRoute>} />
        </Routes>
     </>
   )
