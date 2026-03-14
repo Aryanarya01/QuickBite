126ba3d54eedd19cc76fbf26e8e2f0aee6105dd7
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:41:29 +0530

    Login page logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../api/api";
+import { useAuth } from "../context/AuthContext";
 
 
 const Login = ()=>{
     const navigate = useNavigate();
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
+    const {setUser} = useAuth();
 
     const handleLogin = async (e:React.FormEvent)=>{
         e.preventDefault()
@@ -14,6 +16,13 @@ const Login = ()=>{
             method:"POST",
             body : JSON.stringify({email,password})
         })
+        const data = await res.json();
+        if(res.ok){
+            setUser(data);
+            navigate("/");
+        }else{
+            alert(data.message);
+        }
     }
     return(
         <>
