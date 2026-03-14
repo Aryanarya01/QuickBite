97300f1f690010d80e71ddd553bd71b3529f25d5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:46:23 +0530

    Login page frontend completed

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,41 +3,55 @@ import { Link, useNavigate } from "react-router-dom";
 import { apiFetch } from "../api/api";
 import { useAuth } from "../context/AuthContext";
 
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUser } = useAuth();
 
-const Login = ()=>{
-    const navigate = useNavigate();
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const {setUser} = useAuth();
-
-    const handleLogin = async (e:React.FormEvent)=>{
-        e.preventDefault()
-        const res = await apiFetch("/auth/login",{
-            method:"POST",
-            body : JSON.stringify({email,password})
-        })
-        const data = await res.json();
-        if(res.ok){
-            setUser(data);
-            navigate("/");
-        }else{
-            alert(data.message);
-        }
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const res = await apiFetch("/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      setUser(data);
+      navigate("/");
+    } else {
+      alert(data.message);
     }
-    return(
-        <>
-        <div>
-            <form onSubmit={handleLogin}>
-                <input type="text" placeholder="Enter Email.." value={email} onChange={(e)=>setEmail(e.target.value)} />
-                <br /><br />      
-                <input type="text" placeholder="Enter Password.." value={password} onChange={(e)=>setPassword(e.target.value)} /> 
-                <br /><br />      
-                 <button type="submit">Login</button>
-                 <br /><br />
-                 <p>Don't have an account? <Link to="/register">Register</Link></p>
-            </form>
-        </div>
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <div>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Enter Email.."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Password.."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button type="submit">Login</button>
+          <br />
+          <br />
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
 export default Login;
