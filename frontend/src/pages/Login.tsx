3cc3af816a4787fa187888dcaaf789b5fadffad9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:36:55 +0530

    Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,16 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const Login = ()=>{
+    const navigate = useNavigate();
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
 
+    const handleLogin = (e:React.FormEvent)=>{
+        e.preventDefault()
+        const res
+    }
     return(
         <>
         </>
