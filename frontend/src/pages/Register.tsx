4b3a0ff05677e67d700ca2567e37124ae3de0c5b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 23:26:11 +0530

    handelRegister

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { apiFetch } from "../api/api";
 
 const Register = ()=>{
+    const navigate = useNavigate();
     const [name,setName] = useState("");
     const [email,setEmail] = useState("");
     const [password,setPassword] = useState("");
-    const navigate = useNavigate();
+
+    const handelRegister = async (e:React.FormEvent)=>{
+         e.preventDefault();
+         const res = await apiFetch("/auth/register",{
+            method : "POST",
+            body : JSON.stringify({name,email,password})
+         });
+         const data = await res.json();
+         if(res.ok){
+            
+         }
+    }
     return(
         <>
         </>
