c091dd96daedaa1f93419a7121406213bb5e808d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-14 22:55:47 +0530

    authContext diff func

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, useState, type ReactNode } from "react"
+import { createContext, useEffect, useState, type ReactNode } from "react"
 import type { User } from "../types/User"
 import type { AuthContextType } from "../types/AuthContexTypes";
+import { apiFetch } from "../api/api";
+import { data } from "react-router-dom";
 
 export const AuthContext = createContext<AuthContextType|null>(null)
 export const Authprovider = ({children}:{children : ReactNode})=>{
@@ -9,4 +11,33 @@ export const Authprovider = ({children}:{children : ReactNode})=>{
 
 
     //  Auto login check
+    useEffect(()=>{
+        const checkUser = async ()=>{
+            try{
+                const res = await apiFetch("/auth/profile");
+                if(res.ok){
+                    const data = await res.json();
+                    setUser(data.user);
+                }
+            }catch(err){
+                console.log("Not Logged in!");
+                setUser(null);
+            }finally{
+                setLoading(false);
+            }
+        };
+        checkUser();
+    },[])
+
+
+    const logout = async ()=>{
+        await apiFetch("/auth/logout",{
+            method : "POST"
+        });
+        setUser(null)
+    }
+
+    return(
+
+    )
 }
