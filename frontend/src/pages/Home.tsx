715a9d7c691be76addc46996beeb3d51912ae231
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 10:43:10 +0530

    Home page frontend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
 import type { Food } from "../types/Food";
+import { apiFetch } from "../api/api";
 
 
 const Home = ()=>{
-    const [food,setFood] = useState<Food[]>([]);
+    const [foods,setFoods] = useState<Food[]>([]);
     useEffect(()=>{
         const fetchFood = async ()=>{
+            const data = await apiFetch("/foods");
+            setFoods(data);
         }
+        fetchFood();
     },[])
     return(
         <>
-        
+            <div>
+                <h1>Foods</h1>
+                {
+                    foods.map((food)=>(
+                        <div key={food._id}>
+                            <h3>{food.name}</h3>
+                            <p>{food.price}</p>
+                        </div>
+                    ))
+                }
+            </div>
         </>
     )
 }
