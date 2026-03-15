8f66e4a19748678801ace791f5ace5a207556f3e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 16:07:27 +0530

    CartContextType

--- frontend/src/context/CardContext.tsx
+++ frontend/src/context/CardContext.tsx
@@ -1,2 +1,15 @@
+import { createContext } from "react";
+import type { CartItem } from "../types/Cart";
+import type { Food } from "../types/Food";
 
+interface CartContextType{
+    cart : CartItem[],
+    addToCart : (food:Food)=> void,
+    removeFromCart : (id:string)=>void,
+    increaseQty : (id:string)=>void,
+    decreaseQty : (id:string)=>void,
+    totalPrice : number
 
+}
+
+const CartContext = createContext<()
