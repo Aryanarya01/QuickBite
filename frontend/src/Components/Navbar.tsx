2fac90b197d7b707dffd4db4d28316e240022f8b
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 12:02:51 +0530

    Navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -6,11 +6,25 @@ const Navbar = ()=>{
 
     const {user,logout} = useAuth();
     return(
-        <>
+     
             <nav>
                 <Link to="/">Home</Link>
+
+            {user && (
+                <>
+                <Link to="/cart">Cart</Link>
+                <Link to="/orders">My Orders</Link>
+                <button onClick={logout}>Logout</button>
+                </>
+            )}
+            {!user && (
+                <>
+                <Link to="/login">Login</Link>
+                <Link to="/register"}>Register</Link>
+                </>
+            )}
             </nav>
-        </>
+        
     )
 }
 export default Navbar;
