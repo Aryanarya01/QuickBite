31d990247349d6655636aadf816b4dfc6e97c503
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 10:43:17 +0530

    Home page completed

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,30 +2,27 @@ import { useEffect, useState } from "react";
 import type { Food } from "../types/Food";
 import { apiFetch } from "../api/api";
 
-
-const Home = ()=>{
-    const [foods,setFoods] = useState<Food[]>([]);
-    useEffect(()=>{
-        const fetchFood = async ()=>{
-            const data = await apiFetch("/foods");
-            setFoods(data);
-        }
-        fetchFood();
-    },[])
-    return(
-        <>
-            <div>
-                <h1>Foods</h1>
-                {
-                    foods.map((food)=>(
-                        <div key={food._id}>
-                            <h3>{food.name}</h3>
-                            <p>{food.price}</p>
-                        </div>
-                    ))
-                }
-            </div>
-        </>
-    )
-}
+const Home = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+  useEffect(() => {
+    const fetchFood = async () => {
+      const data = await apiFetch("/foods");
+      setFoods(data);
+    };
+    fetchFood();
+  }, []);
+  return (
+    <>
+      <div>
+        <h1>Foods</h1>
+        {foods.map((food) => (
+          <div key={food._id}>
+            <h3>{food.name}</h3>
+            <p>{food.price}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 export default Home;
