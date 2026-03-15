8d227a86152226df1d0eddd252e8baca2f37bc0e
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 17:30:06 +0530

    Cart page completed

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,21 +1,22 @@
 import { useCart } from "../context/CardContext";
 
-const Cart = ()=>{
-    const {cart,increaseQty,decreaseQty,totalPrice,removeFromCart} = useCart();
-    return(
-        <>
-            {cart.map((item)=>(
-                <div key={item.food._id}>
-                    <h3>{item.food.name}</h3>
-                    <p>{item.food.price}</p>
-                    <button onClick={()=>increaseQty(item.food._id)}>+</button>
-                    {item.quantity}
-                    <button onClick={()=>decreaseQty(item.food._id)}>-</button>
-                    <button onClick={()=>removeFromCart(item.food._id)}>Remove</button>
-                </div>
-            ))}
-            <h2>Total : {totalPrice}</h2>
-        </>
-    )
-}
+const Cart = () => {
+  const { cart, increaseQty, decreaseQty, totalPrice, removeFromCart } =
+    useCart();
+  return (
+    <>
+      {cart.map((item) => (
+        <div key={item.food._id}>
+          <h3>{item.food.name}</h3>
+          <p>{item.food.price}</p>
+          <button onClick={() => increaseQty(item.food._id)}>+</button>
+          {item.quantity}
+          <button onClick={() => decreaseQty(item.food._id)}>-</button>
+          <button onClick={() => removeFromCart(item.food._id)}>Remove</button>
+        </div>
+      ))}
+      <h2>Total : {totalPrice}</h2>
+    </>
+  );
+};
 export default Cart;
