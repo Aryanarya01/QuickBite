fd943333728f5e7d4314066c2db8c5aeac059df5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 22:38:43 +0530

    Checkout page

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import { useCart } from "../context/CardContext";
+import { useNavigate } from "react-router-dom";
 
 
 const Checkout = ()=>{
+    const {cart,totalPrice} = useCart();
+    const [address,setAddress] = useState("");
+    const navigate = useNavigate();
+
+    const handelOrder = async()=>{
+        const items = cart.map((item)=>({
+            food : item.food._id,
+            quantity : item.quantity,
+            price : item.food.price,
+        }))
+        try{
+            await
+        }
+    }
     return(
         <>
         
@@ -8,4 +25,4 @@ const Checkout = ()=>{
         </>
     )
 }
-export default Checkout;
+export default Checkout; 
