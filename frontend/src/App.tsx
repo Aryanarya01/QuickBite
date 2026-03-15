72cfe00a0c6b7c5025a59beb4e9675e3bfe47f67
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 10:47:24 +0530

    App.tsx home route add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,37 @@
- 
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import ProtectedRoute from './Components/ProtectedRoute'
-import Profile from './pages/Profile'
-import Home from './pages/Home'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 
 function App() {
-   
-
   return (
     <>
-       <Routes>
-        <Route path='/register' element={<Register/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/profile' element={<ProtectedRoute>
-          <Profile/>
-        </ProtectedRoute>} />
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
-        <Route path='/' element={<ProtectedRoute>
-          <Home/>
-        </ProtectedRoute>} />
-       </Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
