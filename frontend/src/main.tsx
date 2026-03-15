1e5e80ca761cecc2930309cc2bae0657bcb889d8
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 16:59:19 +0530

    main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { CartProvider } from './context/CardContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
+      <CartProvider>
         <App/>
+      </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
