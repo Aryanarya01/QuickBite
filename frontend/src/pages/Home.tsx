dcfdcd97486f5aac5b5a72d449b1510399c262a2
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 10:39:47 +0530

    Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
+import type { Food } from "../types/Food";
 
 
 const Home = ()=>{
+    const [food,setFood] = useState<Food[]>([]);
+    useEffect(()=>{
+        const fetchFood = async ()=>{
+        }
+    },[])
     return(
         <>
         
