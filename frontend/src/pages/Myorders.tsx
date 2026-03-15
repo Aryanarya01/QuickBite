5eb49ac3021f5a7d7f0dee56bc93ca86c069fe9c
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-15 10:53:42 +0530

    myOrder.tsx

--- frontend/src/pages/Myorders.tsx
+++ frontend/src/pages/Myorders.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react"
+import { apiFetch } from "../api/api";
 
 
 const MyOrder = ()=>{
+    const [orders,setOrders] = useState([]);
+        useEffect(()=>{
+            const fetchOrders = async ()=>{
+                const data = await apiFetch("/orders/my");
+                 setOrders(data);
+            }
+            fetchOrders();
+        },[])
     return(
         <>
         </>
