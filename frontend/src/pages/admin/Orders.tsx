751e0e7d277ae78bbec336c1961b3e11d419f800
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:39:08 +0530

    Orders admin fetchOrders

--- frontend/src/pages/admin/Orders.tsx
+++ frontend/src/pages/admin/Orders.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { apiFetch } from "../../api/api";
 
 const Orders = ()=>{
+    const [orders,setOrders] = useState([]);
+    const fetchOrders =async ()=>{
+        const data = await apiFetch("/orders");
+        setOrders(data);
+    }
     return(
         <>
         
