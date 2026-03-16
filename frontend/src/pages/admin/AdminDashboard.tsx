6e0fb0ca0f61b3d5cf83fb15e876647bc3663b55
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:10:56 +0530

    AdminDashboard

--- frontend/src/pages/admin/AdminDashboard.tsx
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
 
 const AdminDashboard = ()=>{
-    return()
+    return(
+        <div>
+            <h1>Admin Dashboard</h1>
+
+            <Link to="/admin/food-add">
+            <button>Add Food</button>
+            </Link>
+            <Link to="/admin/manage-foods"><button>Manage Foods</button></Link>
+            <Link to="/admin/orders"><button>Orders</button></Link>
+        </div>
+
+    )
 }
 export default AdminDashboard;
