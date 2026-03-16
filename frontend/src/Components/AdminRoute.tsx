a165d1dbf814440e708043352cc55e8e3e0f17d5
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 22:57:51 +0530

    AdminRoute logic

--- frontend/src/Components/AdminRoute.tsx
+++ frontend/src/Components/AdminRoute.tsx
@@ -1,13 +1,17 @@
 import type { JSX } from "react";
 import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 
 const AdminRoute = ({children}:{children : JSX.Element})=>{
 
     const {user,loading} = useAuth();
+    if(loading) return <h1>Loading...</h1>
+    if(!user || user.role !== "admin"){
+       return <Navigate to="/" replace />
+    }
 
-
-    return
+    return children;
 }
 
 export default AdminRoute;
