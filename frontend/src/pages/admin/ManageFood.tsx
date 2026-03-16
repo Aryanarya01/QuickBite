82eb99fa87fc304d9e09d7948bb1a5ce467f6b7a
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:27:33 +0530

    Admin ManageFood

--- frontend/src/pages/admin/ManageFood.tsx
+++ frontend/src/pages/admin/ManageFood.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import { apiFetch } from "../../api/api";
 
 
 const ManageFood = ()=>{
+    const [foods,setFoods] = useState([]);
+    const fetchFood = async ()=>{
+        const data = await apiFetch("/foods");
+        setFoods(data);
+    }
+    
     return(
 
         <>
