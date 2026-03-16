838d0c681f61a29279a3ecbf8987a74fc2fdb507
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 22:23:37 +0530

    improving orders

--- frontend/src/pages/Myorders.tsx
+++ frontend/src/pages/Myorders.tsx
@@ -15,8 +15,11 @@ const MyOrder = () => {
   return (
     <>
       <div>
+        <h1>My Orders...</h1>
+        {orders.length === 0 && <p>No orders yet!</p>}
         {orders.map((order) => (
           <div key={order._id}>
+            <p>Order ID: {order._id}</p>
             <p>Address :{order.address}</p>
             <p>Total : {order.totalPrice}</p>
             <p>Status : {order.status}</p>
