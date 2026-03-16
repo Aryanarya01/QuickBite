80f421e89525699b792bb015ce6d5515d8b07d2f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:31:59 +0530

    Admin ManageFood completed

--- frontend/src/pages/admin/ManageFood.tsx
+++ frontend/src/pages/admin/ManageFood.tsx
@@ -1,36 +1,34 @@
 import { useEffect, useState } from "react";
 import { apiFetch } from "../../api/api";
 
+const ManageFood = () => {
+  const [foods, setFoods] = useState([]);
+  const fetchFood = async () => {
+    const data = await apiFetch("/foods");
+    setFoods(data);
+  };
 
-const ManageFood = ()=>{
-    const [foods,setFoods] = useState([]);
-    const fetchFood = async ()=>{
-        const data = await apiFetch("/foods");
-        setFoods(data);
-    }
-
-    useEffect(()=>{
-        fetchFood();
-    },[])
-    const deleteFood = async(id : string)=>{
-        await apiFetch(`/foods/${id}`,{
-            method:"DELETE"
-        })
-        fetchFood();
-    }
-    return(
-
-        <>
-            <div>
-                <h1>Manage Food</h1>
-                {foods.map((food:any)=>(
-                    <div key={food._id}>
-                        <h3>{food.name}</h3>
-                        <button onClick={()=>deleteFood(food._id)}>Delete</button>
-                    </div>
-                ))}
-            </div>
-        </>
-    )
-}
+  useEffect(() => {
+    fetchFood();
+  }, []);
+  const deleteFood = async (id: string) => {
+    await apiFetch(`/foods/${id}`, {
+      method: "DELETE",
+    });
+    fetchFood();
+  };
+  return (
+    <>
+      <div>
+        <h1>Manage Food</h1>
+        {foods.map((food: any) => (
+          <div key={food._id}>
+            <h3>{food.name}</h3>
+            <button onClick={() => deleteFood(food._id)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 export default ManageFood;
