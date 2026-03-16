ee8ab81087f978860a51a3e768c58d8f618a9c44
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:15:54 +0530

    Admin addfoods handelAdd

--- frontend/src/pages/admin/AddFood.tsx
+++ frontend/src/pages/admin/AddFood.tsx
@@ -4,7 +4,14 @@ import { useState } from "react";
 const AddFood = ()=>{
     const [name,setName] = useState("");
     const [price,setPrice] = useState("");
-    const [description,setDescription] = useState("")
+    const [description,setDescription] = useState("");
+    const [category,setCategory] = useState("");
+
+    const handelAdd =async ()=>{
+        try{
+            
+        }
+    }
     return(
         <>
         
