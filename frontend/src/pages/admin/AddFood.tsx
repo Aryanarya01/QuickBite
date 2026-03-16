61928af2d74e85f253ff3e19932008317784edb9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:25:26 +0530

    Admin addfoods frontend completed

--- frontend/src/pages/admin/AddFood.tsx
+++ frontend/src/pages/admin/AddFood.tsx
@@ -1,42 +1,63 @@
 import { useState } from "react";
 import { apiFetch } from "../../api/api";
 
+const AddFood = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
 
-const AddFood = ()=>{
-    const [name,setName] = useState("");
-    const [price,setPrice] = useState("");
-    const [description,setDescription] = useState("");
-    const [category,setCategory] = useState("");
-
-    const handelAdd =async ()=>{
-        try{
-            await apiFetch("/foods",{
-                method : "POST",
-                body:JSON.stringify({
-                    name,price,description,category
-                })
-            });
-            alert("Food added!");
-            setName("");
-            setPrice("");
-            setDescription("");
-            setCategory("");
-        }catch(err:any){
-            alert(err.message)
-        }
+  const handelAdd = async () => {
+    try {
+      await apiFetch("/foods", {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          price,
+          description,
+          category,
+        }),
+      });
+      alert("Food added!");
+      setName("");
+      setPrice("");
+      setDescription("");
+      setCategory("");
+    } catch (err: any) {
+      alert(err.message);
     }
-    return(
-        <>
-        <div>
-            <h1>Add Food</h1>
-            <input type="text" placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)} />
-            <input type="number" placeholder="Price" value={price} onChange={(e)=>setPrice(e.target.value)} />
-            <input type="text" placeholder="description" value={description} onChange={(e)=>setDescription(e.target.value)} />
-            <input type="text" placeholder="category" value={category} onChange={(e)=>setCategory(e.target.value)} />
-            <button onClick={handelAdd}>Add Food</button>
-        </div>
-        
-        </>
-    )
-}
+  };
+  return (
+    <>
+      <div>
+        <h1>Add Food</h1>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <button onClick={handelAdd}>Add Food</button>
+      </div>
+    </>
+  );
+};
 export default AddFood;
