2bcb0added6526992857a2e7a585e0db1135af31
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-16 23:14:40 +0530

    Admin addfoods states

---
+++ frontend/src/pages/admin/AddFood.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+const AddFood = ()=>{
+    const [name,setName] = useState("");
+    const [price,setPrice] = useState("");
+    const [description,setDescription] = useState("")
+    return(
+        <>
+        
+        </>
+    )
+}
+export default AddFood;
