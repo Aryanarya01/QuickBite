2c77796404497993309c1701998c5eab5bb81e70
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-17 21:44:57 +0530

    Cloudinary setup Route

--- backend/dist/routes/foodRoutes.js
+++ backend/dist/routes/foodRoutes.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 import { isAdmin } from "../middleware/admin.js";
 import { createFood, deleteFood, getAllFood, getFoodById, } from "../Controllers/foodController.js";
 import { Protect } from "../middleware/protect.js";
+import { upload } from "../middleware/upload.js";
 const router = Router();
-router.post("/", Protect, isAdmin, createFood); //admin only
+router.post("/", Protect, isAdmin, upload.single("image"), createFood); //admin only
 router.delete("/:id", Protect, isAdmin, deleteFood); //admin only
 router.get("/", getAllFood);
 router.get("/:id", getFoodById);
--- backend/src/routes/foodRoutes.ts
+++ backend/src/routes/foodRoutes.ts
@@ -7,10 +7,11 @@ import {
   getFoodById,
 } from "../Controllers/foodController.js";
 import { Protect } from "../middleware/protect.js";
+import { upload } from "../middleware/upload.js";
 
 const router: Router = Router();
 
-router.post("/", Protect, isAdmin, createFood); //admin only
+router.post("/", Protect, isAdmin,upload.single("image") ,createFood); //admin only
 router.delete("/:id", Protect, isAdmin, deleteFood); //admin only
 
 router.get("/", getAllFood);
