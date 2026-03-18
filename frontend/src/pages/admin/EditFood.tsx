3ca3c91551d307f5aa067a4ee06cf1e5f8619f77
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-18 21:25:16 +0530

    food editing feature state

--- frontend/src/pages/admin/EditFood.tsx
+++ frontend/src/pages/admin/EditFood.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const EditFood = ()=>{
     const {id} = useParams();
-    
+    const [name,setName] = useState("");
+  const [price,setPrice] = useState("");
+  const [description,setDescription] = useState("");
+  const [category, setCategory] = useState("");
     return(
         <>
         </>
