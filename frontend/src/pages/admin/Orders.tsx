9829e18fe2ff2394d0b89bc535cdb31040d9a799
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-20 01:00:48 +0530

    stylig working on orders

--- frontend/src/pages/admin/Orders.tsx
+++ frontend/src/pages/admin/Orders.tsx
@@ -36,30 +36,32 @@
   };
 
     return (
-      <div>
-        <h1>
+      <div className="min-h-screen bg-[#0f0f0f] text-white p-6">
+        <h1 className="font-bold text-2xl mb-6">
           All Orders 📦
         </h1>
-        <div>
-          <table>
-            <thead>
+        <div className="bg-[#1a1a1a] rounded-lg shadow overflow-hidden">
+          <table className="w-full text-left">
+            <thead className="b-[#111] text-gray-400">
               <tr>
-                <th>Order ID</th>
-                <th>Total</th>
-                <th>Status</th>
-                <th>Update</th>
-                <th>Name</th>
+                <th className="p-3">Order ID</th>
+                <th className="p-3">Total</th>
+                <th className="p-3">Status</th>
+                <th className="p-3">Update</th>
+                <th className="p-3">Name</th>
               </tr>
             </thead>
             <tbody>
               {orders.map((order:any)=>(
-                <tr key={order._id}>
-                  <td>{order._id.slice(-6)}</td>
-                  <td>₹{order.totalPrice}</td>
-                  <td><span className={`px-2 py-1 rounded text-sm ${getStatusColor(order.status)}`}>{order.status}</span></td>
+                <tr key={order._id} className="border-b border-gray-700">
+                  <td className="text-sm p-3">{order._id.slice(-6)}</td>
+                  <td className="p-3">₹{order.totalPrice}</td>
+                  {/* status badge */}
+                  <td className="p-3"><span className={`px-2 py-1 rounded text-sm ${getStatusColor(order.status)}`}>{order.status}</span></td>
                   {/* Dropdown */}
-                  <td>
-                    <select value={order.status} onChange={(e)=>updateStatus(order._id,e.target.value)}>
+                  <td className="p-3">
+                    <select value={order.status} onChange={(e)=>updateStatus(order._id,e.target.value)}
+                      className="bg-[#0f0f0f] p-1 border border-gray-700 outline-none rounded">
                          <option value="Pending">Pending</option>
                     <option value="Preparing">Preparing</option>
                     <option value="Out for delivery">
