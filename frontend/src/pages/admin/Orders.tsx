784c67f6cb313a3df17cfc629d496ea6fdd5336f
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-20 01:00:57 +0530

    stylig working on orders completed

--- frontend/src/pages/admin/Orders.tsx
+++ frontend/src/pages/admin/Orders.tsx
@@ -1,26 +1,25 @@
-  import { useEffect, useState } from "react";
-  import { apiFetch } from "../../api/api";
+import { useEffect, useState } from "react";
+import { apiFetch } from "../../api/api";
 
-  const Orders = () => {
-    const [orders, setOrders] = useState([]);
-    const fetchOrders = async () => {
-      const data = await apiFetch("/orders");
-      setOrders(data);
-    };
-    useEffect(() => {
-      fetchOrders();
-    }, []);
+const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const fetchOrders = async () => {
+    const data = await apiFetch("/orders");
+    setOrders(data);
+  };
+  useEffect(() => {
+    fetchOrders();
+  }, []);
 
-    const updateStatus = async (id: string, status: string) => {
-      await apiFetch(`/orders/${id}`, {
-        method: "PUT",
-        body: JSON.stringify({ status }),
-      });
-      fetchOrders();
-    };
-  
+  const updateStatus = async (id: string, status: string) => {
+    await apiFetch(`/orders/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ status }),
+    });
+    fetchOrders();
+  };
 
- const getStatusColor = (status: string) => {
+  const getStatusColor = (status: string) => {
     switch (status) {
       case "Pending":
         return "bg-gray-600";
@@ -35,49 +34,54 @@
     }
   };
 
-    return (
-      <div className="min-h-screen bg-[#0f0f0f] text-white p-6">
-        <h1 className="font-bold text-2xl mb-6">
-          All Orders 📦
-        </h1>
-        <div className="bg-[#1a1a1a] rounded-lg shadow overflow-hidden">
-          <table className="w-full text-left">
-            <thead className="b-[#111] text-gray-400">
-              <tr>
-                <th className="p-3">Order ID</th>
-                <th className="p-3">Total</th>
-                <th className="p-3">Status</th>
-                <th className="p-3">Update</th>
-                <th className="p-3">Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order:any)=>(
-                <tr key={order._id} className="border-b border-gray-700">
-                  <td className="text-sm p-3">{order._id.slice(-6)}</td>
-                  <td className="p-3">₹{order.totalPrice}</td>
-                  {/* status badge */}
-                  <td className="p-3"><span className={`px-2 py-1 rounded text-sm ${getStatusColor(order.status)}`}>{order.status}</span></td>
-                  {/* Dropdown */}
-                  <td className="p-3">
-                    <select value={order.status} onChange={(e)=>updateStatus(order._id,e.target.value)}
-                      className="bg-[#0f0f0f] p-1 border border-gray-700 outline-none rounded">
-                         <option value="Pending">Pending</option>
+  return (
+    <div className="min-h-screen bg-[#0f0f0f] text-white p-6">
+      <h1 className="font-bold text-2xl mb-6">All Orders 📦</h1>
+      <div className="bg-[#1a1a1a] rounded-lg shadow overflow-hidden">
+        <table className="w-full text-left">
+          <thead className="b-[#111] text-gray-400">
+            <tr>
+              <th className="p-3">Order ID</th>
+              <th className="p-3">Total</th>
+              <th className="p-3">Status</th>
+              <th className="p-3">Update</th>
+              <th className="p-3">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order: any) => (
+              <tr key={order._id} className="border-b border-gray-700">
+                <td className="text-sm p-3">{order._id.slice(-6)}</td>
+                <td className="p-3">₹{order.totalPrice}</td>
+                {/* status badge */}
+                <td className="p-3">
+                  <span
+                    className={`px-2 py-1 rounded text-sm ${getStatusColor(order.status)}`}
+                  >
+                    {order.status}
+                  </span>
+                </td>
+                {/* Dropdown */}
+                <td className="p-3">
+                  <select
+                    value={order.status}
+                    onChange={(e) => updateStatus(order._id, e.target.value)}
+                    className="bg-[#0f0f0f] p-1 border border-gray-700 outline-none rounded"
+                  >
+                    <option value="Pending">Pending</option>
                     <option value="Preparing">Preparing</option>
-                    <option value="Out for delivery">
-                      Out for delivery
-                    </option>
+                    <option value="Out for delivery">Out for delivery</option>
                     <option value="Delivered">Delivered</option>
-                    </select>
-                  </td>
-                  <td>{order.user?.name}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                  </select>
+                </td>
+                <td>{order.user?.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default Orders;
+export default Orders;
