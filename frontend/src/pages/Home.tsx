0478a5c1094a23cd2291cff5d452bc1c2824d600
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-19 22:19:43 +0530

    styling array of objects

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,17 +4,17 @@ import { apiFetch } from "../api/api";
 import { useCart } from "../context/CartContext";
 
 const Home = () => {
-  const { addToCart,cart,totalPrice } = useCart();
+  const { addToCart, cart, totalPrice } = useCart();
   const [foods, setFoods] = useState<Food[]>([]);
   const categories = [
     { name: "All", icon: "🍽️" },
-  { name: "Burger", icon: "🍔" },
-  { name: "Pizza", icon: "🍕" },
-  { name: "Sushi", icon: "🍣" },
-  { name: "Dessert", icon: "🍰" },
-  { name: "Drinks", icon: "🥤" },
-   { name: "Sweet", icon: "🍰" },
-  { name: "INDIAN", icon: "🥤" },
+    { name: "Burger", icon: "🍔" },
+    { name: "Pizza", icon: "🍕" },
+    { name: "Sushi", icon: "🍣" },
+    { name: "Dessert", icon: "🍰" },
+    { name: "Drinks", icon: "🥤" },
+    { name: "Sweet", icon: "🍰" },
+    { name: "INDIAN", icon: "🥤" },
   ];
   const [activeCategory, setActiveCategory] = useState("All");
   useEffect(() => {
@@ -58,44 +58,47 @@ const Home = () => {
           {/* Category selector */}
           <div className="flex gap-4 overflow-x-auto mb-8">
             {categories.map((cat) => (
-                 <div key={cat.name} onClick={()=>setActiveCategory(cat.name)}  className="flex flex-col items-center cursor-pointer">
-                    {/* Circle */}
-      <div
-        className={`w-16 h-16 flex items-center justify-center rounded-full text-2xl transition
+              <div
+                key={cat.name}
+                onClick={() => setActiveCategory(cat.name)}
+                className="flex flex-col items-center cursor-pointer"
+              >
+                {/* Circle */}
+                <div
+                  className={`w-16 h-16 flex items-center justify-center rounded-full text-2xl transition
         ${
           activeCategory === cat.name
             ? "bg-orange-500 text-white shadow-[0_0_15px_rgba(255,115,0,0.7)]"
             : "bg-[#1a1a1a] text-gray-300"
         }`}
-      >
-        {cat.icon}
-      </div>
+                >
+                  {cat.icon}
+                </div>
 
-      {/* Label */}
-      <p
-        className={`mt-2 text-sm ${
-          activeCategory === cat.name
-            ? "text-orange-500"
-            : "text-gray-400"
-        }`}
-      >
-        {cat.name}
-      </p>
+                {/* Label */}
+                <p
+                  className={`mt-2 text-sm ${
+                    activeCategory === cat.name
+                      ? "text-orange-500"
+                      : "text-gray-400"
+                  }`}
+                >
+                  {cat.name}
+                </p>
               </div>
             ))}
           </div>
-           
 
           {/* foodGrid */}
           <div className="grid grid-cols-4 gap-6">
             {filteredFoods.map((food) => (
-            <div
-  key={food._id}
-  className="bg-[#1a1a1a] p-4 rounded-lg shadow-md 
-  hover:shadow-orange-500/40 
-  hover:scale-105 
-  transition duration-300 ease-in-out cursor-pointer"
->
+              <div
+                key={food._id}
+                className="bg-[#1a1a1a] p-4 rounded-lg shadow-md 
+                           hover:shadow-orange-500/40 
+                            hover:scale-105 
+                            transition duration-300 ease-in-out cursor-pointer"
+              >
                 <img
                   src={food.image}
                   className="w-full h-40 object-cover rounded"
@@ -114,34 +117,32 @@ const Home = () => {
           </div>
         </div>
 
-
-
-
         {/* Right Cart */}
         <div className="w-72 bg-[#1a1a1a] p-5 shadow-lg">
           <h2 className="text-xl font-semibold mb-4">Your Feast 🍔</h2>
 
-           {
-            cart.length === 0 ? (
-              <p className="text-gray-500">Cart is empty...</p>
-            ) : (
-              <div className="space-y-4" >
-                {cart.map((item)=>(
-                  <div key={item.food._id} className="flex justify-between items-center">
-                    <div>
-                      <p className="font-medium">{item.food.name}</p>
-                      <p className="text-sm text-gray-500">x{item.quantity}</p>
-                    </div>
-                     <p className="text-sm">
-            ₹{item.food.price * item.quantity}
-          </p>
+          {cart.length === 0 ? (
+            <p className="text-gray-500">Cart is empty...</p>
+          ) : (
+            <div className="space-y-4">
+              {cart.map((item) => (
+                <div
+                  key={item.food._id}
+                  className="flex justify-between items-center"
+                >
+                  <div>
+                    <p className="font-medium">{item.food.name}</p>
+                    <p className="text-sm text-gray-500">x{item.quantity}</p>
                   </div>
-                ))}
-              </div>
-            )
-           }
-           {/* total */}
-           <div className="mt-6 border-t pt-4"><p className="font-semibold">Total : ₹{totalPrice} </p></div>
+                  <p className="text-sm">₹{item.food.price * item.quantity}</p>
+                </div>
+              ))}
+            </div>
+          )}
+          {/* total */}
+          <div className="mt-6 border-t pt-4">
+            <p className="font-semibold">Total : ₹{totalPrice} </p>
+          </div>
 
           <button className="mt-6 w-full bg-orange-500 text-white py-2 rounded">
             View Cart
