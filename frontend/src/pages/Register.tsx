b32edb2a19a2d39de326c78c1c29457e8aac69f9
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-19 22:58:30 +0530

    styling login,register

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -23,24 +23,27 @@ const Register = () => {
   //   }
   // };
   const handelRegister = async (e: React.FormEvent) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  try {
-    await apiFetch("/auth/register", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-    });
+    try {
+      await apiFetch("/auth/register", {
+        method: "POST",
+        body: JSON.stringify({ name, email, password }),
+      });
 
-    alert("Registered Successfully! Please Login....");
-    navigate("/login");
-  } catch (err: any) {
-    alert(err.message);
-  }
-};
+      alert("Registered Successfully! Please Login....");
+      navigate("/login");
+    } catch (err: any) {
+      alert(err.message);
+    }
+  };
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-300">
-        <form className="bg-white p-6 rounded-lg shadow w-80" onSubmit={handelRegister}>
+        <form
+          className="bg-white p-6 rounded-lg shadow w-80"
+          onSubmit={handelRegister}
+        >
           <h2 className="text-xl font-bold mb-4">Register</h2>
           <input
             type="text"
@@ -69,11 +72,19 @@ const Register = () => {
           />
           <br />
           <br />
-          <button className="w-full bg-orange-500 rounded py-2 text-white" type="submit">Register</button>
+          <button
+            className="w-full bg-orange-500 rounded py-2 text-white"
+            type="submit"
+          >
+            Register
+          </button>
           <br />
           <br />
           <p className="text-sm mt-2">
-            Already have Account? <Link className="text-blue-500" to="/login">Login</Link>
+            Already have Account?{" "}
+            <Link className="text-blue-500" to="/login">
+              Login
+            </Link>
           </p>
         </form>
       </div>
