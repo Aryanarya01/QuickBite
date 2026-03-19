afee75db1660b432a2044b5a0835d2e27506a286
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-20 00:18:21 +0530

    styling working on AddFood->Admin

--- frontend/src/pages/admin/AddFood.tsx
+++ frontend/src/pages/admin/AddFood.tsx
@@ -39,36 +39,41 @@ const AddFood = () => {
     <>
       <div className="min-h-screen bg-[#0f0f0f] text-white flex items-center justify-center">
         <div className="bg-[#1a1a1a] p-6 rounded-lg w-96 shadow">
-          <h1 className="">Add Food 🍔</h1>
+          <h1 className="text-xl font-bold mb-4">Add Food 🍔</h1>
           <input
             type="text"
             placeholder="Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            className="w-full p-2 mb-3 border bg-[#0f0f0f] outline-none rounded border-gray-700"
           />
           <input
             type="file"
             onChange={(e) => setImage(e.target.files?.[0] || null)}
+            className="w-full text-sm mb-3 text-gray-400"
           />
           <input
             type="number"
             placeholder="Price"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
+            className="w-full p-2 mb-3 border bg-[#0f0f0f] outline-none rounded border-gray-700"
           />
           <input
             type="text"
             placeholder="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            className="w-full p-2 mb-3 border bg-[#0f0f0f] outline-none rounded border-gray-700"
           />
           <input
             type="text"
             placeholder="category"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
+            className="w-full p-2 mb-3 border bg-[#0f0f0f] outline-none rounded border-gray-700"
           />
-          <button onClick={handelAdd}>Add Food</button>
+          <button className="w-full bg-orange-500 hover:bg-orange-600 rounded transition py-2" onClick={handelAdd}>Add Food</button>
         </div>
       </div>
     </>
