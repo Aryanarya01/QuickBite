81a6b379250a90f52ddadd619fb3c401e709fb95
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-20 22:14:49 +0530

    stylig working on navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -4,22 +4,25 @@ import { useAuth } from "../context/AuthContext";
 const Navbar = () => {
   const { user, logout } = useAuth();
   return (
-    <nav>
-      <Link to="/">Home</Link>
+    <nav className="flex items-center justify-between px-6 py-3 text-white bg-[#1a1a1a]">
+      {/* Left */}
+      <Link to="/" className="text-xl font-bold text-orange-500">QuickBite</Link>
 
-      {user && (
-        <>
-          <Link to="/cart">Cart</Link>
-          <Link to="/orders">My Orders</Link>
-          <button onClick={logout}>Logout</button>
-        </>
-      )}
-      {!user && (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
+    {/* Right */}
+      <div className="flex gap-4 items-center">
+        {user && (
+          <>
+        
+            <button className="bg-orange-500 rounded py-1 px-4 hover:bg-orange-600" onClick={logout}>Logout</button>
+          </>
+        )}
+        {!user && (
+          <>
+            <Link className="hover:text-orange-500" to="/login">Login</Link>
+            <Link className="hover:text-orange-500" to="/register">Register</Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
