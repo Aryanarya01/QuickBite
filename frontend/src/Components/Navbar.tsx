f32c6b2a26de6e59b625a1b6dbe330393da70e4d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-20 22:15:07 +0530

    stylig working on navbar completed

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -6,19 +6,30 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 py-3 text-white bg-[#1a1a1a]">
       {/* Left */}
-      <Link to="/" className="text-xl font-bold text-orange-500">QuickBite</Link>
+      <Link to="/" className="text-xl font-bold text-orange-500">
+        QuickBite
+      </Link>
 
-    {/* Right */}
+      {/* Right */}
       <div className="flex gap-4 items-center">
         {user && (
           <>
-            <button className="bg-orange-500 rounded py-1 px-4 hover:bg-orange-600" onClick={logout}>Logout</button>
+            <button
+              className="bg-orange-500 rounded py-1 px-4 hover:bg-orange-600"
+              onClick={logout}
+            >
+              Logout
+            </button>
           </>
         )}
         {!user && (
           <>
-            <Link className="hover:text-orange-500" to="/login">Login</Link>
-            <Link className="hover:text-orange-500" to="/register">Register</Link>
+            <Link className="hover:text-orange-500" to="/login">
+              Login
+            </Link>
+            <Link className="hover:text-orange-500" to="/register">
+              Register
+            </Link>
           </>
         )}
       </div>
