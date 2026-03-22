b60fca40def5c923973265400f4f464e96c1ed89
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-22 21:49:31 +0530

    admin dashboard manage food and orders stying enhanced

--- frontend/src/pages/admin/AdminDashboard.tsx
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -17,7 +17,7 @@ const AdminDashboard = () => {
         {/* Manage food */}
         <Link to="/admin/manage-foods">
           <div className="bg-[#1a1a1a] p-6 rounded-xl shadow hover:shadow-orange-500/30 hover:scale-105 text-center curser-pointer transition">
-             <h2 className="text-xl font-semibold mb-2"></h2>
+             <h2 className="text-xl font-semibold mb-2">🍔 Manage Food</h2>
              <p className="text-sm text-gray-400">Edit or delete food items</p>
           </div>
         </Link>
@@ -25,7 +25,7 @@ const AdminDashboard = () => {
         {/* orders */}
         <Link to="/admin/orders">
           <div className="bg-[#1a1a1a] p-6 rounded-xl shadow hover:shadow-orange-500/30 hover:scale-105 text-center curser-pointer transition">
-             <h2 className="text-xl font-semibold mb-2"></h2>
+             <h2 className="text-xl font-semibold mb-2">📦 Orders</h2>
              <p className="text-sm text-gray-400"></p>
           </div>
         </Link>
