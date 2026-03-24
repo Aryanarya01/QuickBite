c02606aca9563c02cd87ff5cf0cfd2a569f6107d
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-24 22:25:53 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,6 +16,7 @@ app.use(
   cors({
 origin: [
     "http://localhost:5173",
+    "https://quickbite-q5a95l1po-aryanarya01s-projects.vercel.app",
     "https://quickbite-git-main-aryanarya01s-projects.vercel.app"
   ],
     credentials: true,
