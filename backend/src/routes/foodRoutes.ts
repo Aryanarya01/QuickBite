b9e9034adf505e045004c406f2a79a5580be19fe
Aryanarya01 <aryanaryaa2006@gmail.com>
2026-03-31 21:31:53 +0530

    adding search functionality

--- backend/src/routes/foodRoutes.ts
+++ backend/src/routes/foodRoutes.ts
@@ -9,6 +9,7 @@ import {
 } from "../Controllers/foodController.js";
 import { Protect } from "../middleware/protect.js";
 import { upload } from "../middleware/upload.js";
+import { Food } from "../models/foodModel.js";
 
 const router: Router = Router();
 
@@ -17,5 +18,13 @@ router.delete("/:id", Protect, isAdmin, deleteFood); //admin only
 router.put("/:id", Protect, isAdmin, upload.single("image"), updateFood); //admin only
 router.get("/", getAllFood);
 router.get("/:id", getFoodById);
+router.get("/search", async (req, res) => {
+  const q = req.query.q as string;
 
+  const foods = await Food.find({
+    name: { $regex: q, $options: "i" },
+  });
+
+  res.json(foods);
+});
 export default router;
